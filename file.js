53d80acdca185c80f31f99ff6366732a1b20d200
Ruhul <ruhulthisis@gmail.com>
2025-05-09 22:37:37 +0600

    how to delete,make folder, copy file

--- file.js
+++ file.js
@@ -2,23 +2,50 @@ const fs = require("fs");
 
 // how to create file
 // sync
-fs.writeFileSync("./test.txt","hey there")
+// fs.writeFileSync("./test.txt","hey there")
 
 // async
-fs.writeFile("./test.txt","hey there asyn", (err) => {})
+// fs.writeFile("./test.txt","hey there asyn", (err) => {})
 
 
 // how to read file
 // sync
 
-const result = fs.readFileSync("./contacts.txt", "utf-8");
-console.log(result);
+// const result = fs.readFileSync("./contacts.txt", "utf-8");
+// console.log(result);
 
 // async
-fs.readFile("./contacts.txt", "utf-8", (err, result) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log(result);
-});
+// fs.readFile("./contacts.txt", "utf-8", (err, result) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log(result);
+// });
+
+// how to apend file
+
+// fs.appendFileSync("./test.txt",`Ruhul\n`);
+
+
+
+// how to copy file
+
+// sync
+// fs.copyFileSync("./test.txt", "./copy.txt");
+
+// async
+// fs.copyFile("./test.txt", "./copy.txt", (err) => {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     console.log("file copied");
+// });
+
+// how to delete file
+// fs.unlinkSync("./test.txt");
+
+// how make folder
+
+fs.mkdirSync("./my-docs");
