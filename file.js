5511a35af2bef134ce2880b4275f60dbdfc591e0
Ruhul <ruhulthisis@gmail.com>
2025-05-09 22:23:36 +0600

    how to read file

--- file.js
+++ file.js
@@ -1,8 +1,24 @@
 const fs = require("fs");
 
-
+// how to create file
 // sync
-// fs.writeFileSync("./test.txt","hey there")
+fs.writeFileSync("./test.txt","hey there")
 
 // async
 fs.writeFile("./test.txt","hey there asyn", (err) => {})
+
+
+// how to read file
+// sync
+
+const result = fs.readFileSync("./contacts.txt", "utf-8");
+console.log(result);
+
+// async
+fs.readFile("./contacts.txt", "utf-8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(result);
+});
